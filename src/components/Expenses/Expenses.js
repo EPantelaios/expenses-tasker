7cc77ea8b5b9b350bb83125d018df8a52561986b
Evripidis Pantelaios <euripidis_pantelaios@hotmail.com>
2022-10-10 17:43:17 +0300

    feat: can filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import FilterByYear from './FilterByYear';
 import './Expenses.css';
 
 const Expenses = ({ props }) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
-      {props.map((_, index) => (
-        <ExpenseItem
-          title={props[index].title}
-          amount={props[index].amount}
-          date={props[index].date}
-        />
-      ))}
+      <h2 style={{ color: 'grey', textAlign: 'center' }}>
+        Selected Year: {filteredYear}
+      </h2>
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* <FilterByYear props={props} /> */}
+      {props.map(
+        (item) =>
+          item.title !== '' &&
+          item.amount !== '' &&
+          item.date.getFullYear() == filteredYear && (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          )
+      )}
     </Card>
   );
 };
