86cd945340d474a676072580a0c92bcabc11a241
Evripidis Pantelaios <euripidis_pantelaios@hotmail.com>
2022-10-10 20:58:08 +0300

    feat: change state for menu option for adding a new expense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './ExpenseForm.css';
 
@@ -6,6 +6,8 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
+  const [showMenuForAddExpense, setShowMenuForAddExpense] = useState(false);
+  const [disableButton, setDisableButton] = useState(true);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -32,44 +34,93 @@ const ExpenseForm = (props) => {
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
+    setShowMenuForAddExpense((prevState) => !prevState);
+    setDisableButton(true);
   };
 
+  const handlerInitialMenu = (event) => {
+    event.preventDefault();
+
+    setDisableButton(true);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+    setShowMenuForAddExpense((prevState) => !prevState);
+  };
+
+  const hoverButtonMessage = disableButton
+    ? 'Fill all input fields and then press this button'
+    : 'Press the button to add new expense';
+
+  useEffect(() => {
+    if (enteredTitle && enteredAmount && enteredDate) setDisableButton(false);
+  }, [enteredAmount, enteredDate, enteredTitle]);
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <>
+      {showMenuForAddExpense ? (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+                onKeyPress={(e) => {
+                  e.key === 'Enter' && e.preventDefault();
+                }}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+                onKeyPress={(e) => {
+                  e.key === 'Enter' && e.preventDefault();
+                }}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+                onKeyPress={(e) => {
+                  e.key === 'Enter' && e.preventDefault();
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="new-expense__buttons">
+            <div className="new-expense__actions">
+              <button onClick={handlerInitialMenu}>Cancel</button>
+            </div>
+            <div className="new-expense__actions">
+              <button
+                disabled={disableButton}
+                title={hoverButtonMessage}
+                type="submit"
+              >
+                Add Expense
+              </button>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div className="new-expense__nodialog">
+          <button onClick={handlerInitialMenu}>Add New Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      )}
+    </>
   );
 };
 
