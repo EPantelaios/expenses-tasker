45ee33f1c7327d3c9631bc8bd480bb18e8d7435e
evripant <evripidispantelaios@gmail.com>
2022-10-30 22:28:24 +0200

    feature: add chart bar for visualizing expenses value per month

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,23 @@
+import ChartBar from './ChartBar';
+import _ from 'lodash';
+import './Chart.css';
+
+const Chart = (props) => {
+  const dataPointsValue = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalSum = _.sum(dataPointsValue);
+
+  const chartBars = props.dataPoints.map((dataPoint, index) => {
+    return (
+      <ChartBar
+        key={index}
+        value={dataPoint.value}
+        label={dataPoint.label}
+        totalSum={totalSum}
+      />
+    );
+  });
+
+  return <div className="chart">{chartBars}</div>;
+};
+
+export default Chart;
